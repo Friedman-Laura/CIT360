26e5c56395c21c66a46f6c81ac849516b7fb64ae
Laura Friedman <fri16006@byui.edu>
2018-05-26 21:22:06 -0500

    Added Threads, Executors & Runnables Added JSON

---
+++ src/ThreadsExecutorsRunnables/App4.java
@@ -0,0 +1,19 @@
+package ThreadsExecutorsRunnables;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Laura
+ */
+public class App4 {
+    
+    public static void main(String[] args) {
+        new Worker().main();
+    }
+    
+}
